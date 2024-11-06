9dcf9353c4e72ac74f80176ee47a6063860f3f54
hiteshwadhwani <developerhitesh1403@gmail.com>
2024-11-06 20:27:36 +0530

    added cli commands to do actions

--- cmd/main.go
+++ cmd/main.go
@@ -3,46 +3,94 @@
 
 package main
 
-import "hiteshwadhwani/bitcask-datstore.git/internal/bitcask"
+import (
+	"fmt"
+	"hiteshwadhwani/bitcask-datstore.git/internal/bitcask"
+	"hiteshwadhwani/bitcask-datstore.git/pkg"
+	"os"
+)
 
 func main() {
-	// db, err := bitcask.NewDiskStore("bitcask.db")
+	db, err := bitcask.NewDiskStore("bitcask.db")
 
-	// if err != nil {
-	// 	fmt.Printf("something went wrong: %v", err)
-	// }
+	if err != nil {
+		fmt.Printf("something went wrong: %v", err)
+	}
 
-	// defer db.Close()
-	// defer os.Remove("bitcask.db")
+	defer db.Close()
+	defer os.Remove("bitcask.db")
 
-	// if err != nil {
-	// 	fmt.Printf("error creating disk store: %v", err)
-	// }
+	var commands = []pkg.Command{
+		{
+			Name: "set",
+			Run: func(args []string) {
+				if len(args) < 2 {
+					fmt.Println("set command requires a key and a value")
+					return
+				}
 
-	// err = db.Set("name", "hitesh")
+				key, value := args[0], args[1]
 
-	// if err != nil {
-	// 	fmt.Printf("error putting value: %v", err)
-	// }
+				err := db.Set(key, value)
+				if err != nil {
+					fmt.Printf("error setting value: %v \n", err)
+				}
 
-	// value, err := db.Get("name")
-	// if err != nil {
-	// 	fmt.Printf("error getting value: %v", err)
-	// }
+				fmt.Printf("value set successfully: %v \n", value)
+			},
+		},
+		{
+			Name: "get",
+			Run: func(args []string) {
+				if len(args) < 1 {
+					fmt.Println("get command requires a key")
+					return
+				}
 
-	// fmt.Println(value)
+				key := args[0]
 
-	// err = db.Delete("name")
-	// if err != nil {
-	// 	fmt.Printf("error deleting value: %v", err)
-	// }
+				value, err := db.Get(key)
 
-	// value, err = db.Get("name")
-	// if err != nil {
-	// 	fmt.Printf("error getting value: %v", err)
-	// }
+				if err != nil {
+					fmt.Printf("error getting value: %v \n", err)
+					return
+				}
 
-	// fmt.Println(value)
+				fmt.Printf("value: %v \n", value)
+			},
+		},
+		{
+			Name: "delete",
+			Run: func(args []string) {
+				if len(args) < 1 {
+					fmt.Println("delete command requires a key")
+					return
+				}
 
-	bitcask.Execute()
+				key := args[0]
+
+				err := db.Delete(key)
+				if err != nil {
+					fmt.Printf("error deleting value: %v \n", err)
+					return
+				}
+
+				fmt.Println("value deleted successfully")
+			},
+		},
+		{
+			Name: "exit",
+			Run: func(args []string) {
+				os.Exit(0)
+			},
+		},
+	}
+
+	cliSession := pkg.NewInteractiveSession()
+
+	for _, command := range commands {
+		cliSession.AddCommand(command)
+	}
+	cliSession.StartInteractiveSession()
+
 }
--- pkg/utils.go
+++ pkg/utils.go
@@ -5,10 +5,28 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
-func RunInteractiveMode() error {
+type InteractiveSession struct {
+	commands []Command
+}
+
+type Command struct {
+	Name string
+	Run  func(args []string)
+}
+
+func NewInteractiveSession() *InteractiveSession {
+	interactiveSession := &InteractiveSession{
+		commands: make([]Command, 0),
+	}
+
+	return interactiveSession
+}
+
+func (i *InteractiveSession) StartInteractiveSession() {
 	fmt.Println("Welcome to interactive mode. Press Ctrl+C to exit.")
 
 	// Set up signal handling for Ctrl+C
@@ -22,14 +40,42 @@ func RunInteractiveMode() error {
 		os.Exit(0)
 	}()
 
+	defer close(sigChan)
+
 	// Start the interactive loop
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("> ")
 		if scanner.Scan() {
 			input := scanner.Text()
-			// Process the input here
-			fmt.Printf("You entered: %s\n", input)
+			i.HandleCommands(input)
 		}
 	}
+}
+
+func (i *InteractiveSession) HandleCommands(text string) {
+	// use cobra to handle commands
+	command, userArgs := strings.Split(text, " ")[0], strings.Split(text, " ")[1:]
+
+	flag := false
+
+	if command == "" {
+		fmt.Println("Invalid command")
+		return
+	}
+
+	for _, cmd := range i.commands {
+		if cmd.Name == command {
+			flag = true
+			cmd.Run(userArgs)
+		}
+	}
+
+	if !flag {
+		fmt.Println("Invalid command")
+	}
+}
+
+func (i *InteractiveSession) AddCommand(command Command) {
+	i.commands = append(i.commands, command)
 }
